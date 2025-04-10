1f10417a33408c931468d787928afc28b1df34ed
Gabor Angyal <gabor.angyal@codesharp.hu>
2025-04-10 15:09:45 +0200

    fix linter issues

--- prerender.js
+++ prerender.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { renderToString } from '@vue/server-renderer';
 import { spawn } from 'child_process';
+import process from 'node:process';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import process from 'node:process'
 
 // https://vite.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(() => {
   const isSSR = process.env.SSR === 'true';
   
   return {
